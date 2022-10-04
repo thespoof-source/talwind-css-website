3f57c05e3b1b590ed72f969bdc2dc987a0287dcd
SK ARBAZ ALAM <thespoof-source>
2022-10-04 06:08:20 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./pages/**/*.{js, jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        FredokaOne: "Fredoka One",
+      },
+    },
+  },
+  plugins: [],
+};
